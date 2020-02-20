c6954e7e571fc177db1d127b16c7ed998f7e82a4
gezhang <oscezrcd@gmail.com>
2020-02-20 09:49:34 -0500

    limit rearrangement to 1 time step, rearrangements happen sporadically and no avalanche. If I let each rearrangement last 5 frames, then avalanches does not stop

---
+++ src/gridModel.cpp
@@ -0,0 +1,179 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <cmath>
+
+class gridModel
+{
+public:
+    int nGridPerSide;
+    double lGrid;
+    int bufferCenter;
+
+    std::vector<double> dEBuffer, dSBuffer, alle, alls; //e is strain
+    std::vector<char> rearrangingStep;
+    std::vector<bool> hasRearranged;
+
+    std::mt19937 rEngine;
+    std::normal_distribution<double> distriBution;
+
+    gridModel(int nGrid, double lGrid) : rEngine(0), distriBution(-2.0, 2.0), nGridPerSide(nGrid), lGrid(lGrid)
+    {
+    }
+
+    bool startRearranging(double e, double s)
+    {
+        return e > 0.07 - 0.01 * s;
+    }
+    double eFromRearranger(double dx, double dy, double r)
+    {
+        if (r < 1.0)
+            return 3e-2;
+        else
+            return 3e-2 / r / r;
+    }
+    double dsFromRearranger(double dx, double dy, double r)
+    {
+        if (r < 4.0)
+            return 0.1;
+        else if (r < 30)
+            return 1.0 / r / r / r;
+        else
+            return 0.0;
+    }
+    void getBuffer()
+    {
+        bufferCenter = nGridPerSide / 2;
+        dEBuffer.resize(nGridPerSide * nGridPerSide);
+        dSBuffer.resize(nGridPerSide * nGridPerSide);
+        for (int i = 0; i < nGridPerSide; i++)
+            for (int j = 0; j < nGridPerSide; j++)
+            {
+                double dx = (i - bufferCenter) * lGrid;
+                double dy = (j - bufferCenter) * lGrid;
+                double r = std::sqrt(dx * dx + dy * dy);
+                int index = i * nGridPerSide + j;
+                dEBuffer[index] = eFromRearranger(dx, dy, r);
+                dSBuffer[index] = dsFromRearranger(dx, dy, r);
+            }
+    }
+    void initialize()
+    {
+        int nSite = nGridPerSide * nGridPerSide;
+        alle.resize(nSite);
+        alls.resize(nSite);
+        hasRearranged.resize(nSite);
+        rearrangingStep.resize(nSite);
+        for (int i = 0; i < nSite; i++)
+        {
+            this->alle[i] = 0.0;
+            this->alls[i] = this->distriBution(this->rEngine);
+            this->hasRearranged[i] = 0;
+            this->rearrangingStep[i] = 0;
+        }
+        this->getBuffer();
+    }
+    void shear()
+    {
+        int nSite = nGridPerSide * nGridPerSide;
+        for (int i = 0; i < nSite; i++)
+        {
+            this->alle[i] += 1e-5;
+            this->hasRearranged[i] = 0;
+            this->rearrangingStep[i] = 0;
+        }
+    }
+    bool avalanche()
+    {
+        bool avalancheHappened = false;
+        int nSite = nGridPerSide * nGridPerSide;
+#pragma omp parallel
+        {
+            int numRearrange = 1;
+            while (numRearrange > 0)
+            {
+#pragma omp for schedule(static)
+                for (int i = 0; i < nSite; i++)
+                    if (startRearranging(alle[i], alls[i]))
+                    {
+                        hasRearranged[i] = 1;
+                        rearrangingStep[i] = 1;
+                    }
+                numRearrange = 0;
+                for (int i = 0; i < nSite; i++)
+                {
+                    if (rearrangingStep[i] > 0)
+                    {
+                        //update softness and strain
+                        int rx = i / nGridPerSide;
+                        int ry = i % nGridPerSide;
+#pragma omp for schedule(static)
+                        for (int x = 0; x < nGridPerSide; x++)
+                        {
+                            int xInBuffer = bufferCenter - rx + x;
+                            while (xInBuffer < 0)
+                                xInBuffer += nGridPerSide;
+                            while (xInBuffer >= nGridPerSide)
+                                xInBuffer -= nGridPerSide;
+                            for (int y = 0; y < nGridPerSide; y++)
+                            {
+                                int yInBuffer = bufferCenter - ry + y;
+                                while (yInBuffer < 0)
+                                    yInBuffer += nGridPerSide;
+                                while (yInBuffer >= nGridPerSide)
+                                    yInBuffer -= nGridPerSide;
+                                //alle[x * nGridPerSide + y] += dEBuffer[xInBuffer * nGridPerSide + yInBuffer];
+                                double & e = alle[x*nGridPerSide+y];
+                                double & de = dEBuffer[xInBuffer*nGridPerSide+yInBuffer];
+                                e=std::sqrt(e*e+de*de);
+                                alls[x * nGridPerSide + y] += dSBuffer[xInBuffer * nGridPerSide + yInBuffer];
+                            }
+                        }
+                        numRearrange++;
+                    }
+                }
+#pragma omp single 
+                {
+                    for (int i = 0; i < nSite; i++)
+                    {
+                        if (rearrangingStep[i] > 0)
+                        {
+                            //carry out the rearrangement
+                            rearrangingStep[i]++;
+                            if (rearrangingStep[i] > 0)
+                            {
+                                rearrangingStep[i] = 0;
+                                alle[i]=0.0;
+                                alls[i]=distriBution(rEngine);
+                            }
+                        }
+                    }
+
+                    if (numRearrange > 0)
+                    {
+                        avalancheHappened = true;
+                        std::cout << "num rearranger in this frame=" << numRearrange << std::endl;
+                    }
+                }
+            }
+        }
+        return avalancheHappened;
+    }
+};
+
+int main()
+{
+    gridModel model(300, 1.0);
+    model.initialize();
+    int numAvalanche = 0;
+    while (numAvalanche < 1000)
+    {
+        std::cout << "shearing\n";
+        model.shear();
+        std::cout << "checking avalanche\n";
+        numAvalanche += model.avalanche();
+        std::cout << numAvalanche << "avalanches so far.\n";
+    }
+    for (auto &s : model.alls)
+        std::cout << s << std::endl;
+}
